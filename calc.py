36b4e8c783464f842acfb8ef938334e94c62474f
Adan Zamorano <azamorano@miners.utep.edu>
2025-06-30 17:06:12 +0000

    added yaml file

---
+++ calc.py
@@ -0,0 +1,27 @@
+class Calc:
+    """A simple calculator class"""
+
+    def add(self, a, b):
+        """Add two numbers together"""
+        return a + b
+
+    def sub(self, a, b):
+        """Subtract two numbers"""
+        return a - b
+    
+    def mul(self, a, b):
+        """Multiply two numbers"""
+        return a * b
+    
+    def div(self, a, b):
+        """Divide two numbers"""
+        return a / b
+    
+    def pow(self, a, b):
+        """Raise a to the power of b"""
+        return a ** b
+    
+    def sqrt(self, a):
+        """Return the square root of a"""
+        return a ** 0.5
+    
